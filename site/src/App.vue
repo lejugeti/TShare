9fa9a7d46652a280d0ce6aaea9c937425bf77bd7
Antoine PARIZE <45354189+lejugeti@users.noreply.github.com>
2020-10-28 12:17:23 +0100

    Re-création du projet à cause de bugs

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <p>HELLO</p>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: 'App',
+};
 </script>
 
 <style>
