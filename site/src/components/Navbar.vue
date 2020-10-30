030bc296790568711d50c3b88f844a2fc762198d
ctriat <c.triat33@gmail.com>
2020-10-30 20:08:15 +0100

    Rebase main

--- site/src/components/Navbar.vue
+++ site/src/components/Navbar.vue
@@ -1,10 +1,16 @@
 <template>
   <b-navbar toggleable="lg" type="" variant="faded">
     <b-navbar-brand to="/">
+<<<<<<< HEAD
       <img id="logo" src="./../assets/logo.png" @click="navToDefaultPage"/>
     </b-navbar-brand>
     <b-navbar-nav class="ml-auto text">
       <b-nav-item to="/navbar" class=" mr-4" @click="navToAjoutPage">Ajouter un vêtement</b-nav-item>
+=======
+      <img id="logo" src="./../assets/logo.png"/>
+    </b-navbar-brand>
+    <b-navbar-nav class="ml-auto text">
+>>>>>>> 9364028 (Rebase main)
       <b-nav-item to="/navbar" class=" mr-4">Créer mon compte</b-nav-item>
       <b-nav-item to="/navbar" class=" mr-4">Se connecter</b-nav-item>
     </b-navbar-nav>
@@ -13,6 +19,7 @@
 
 <script>
 export default {
+<<<<<<< HEAD
   name: "navbar",
   methods: {
     navToDefaultPage(){
@@ -22,6 +29,9 @@ export default {
       this.$router.push("/ajoutVetement")
     }
   }
+=======
+  name: "navbar"
+>>>>>>> 9364028 (Rebase main)
 };
 </script>
 
