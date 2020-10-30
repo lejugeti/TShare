e659bf417f80c2f6822e8763ccc9669a8b69a496
ctriat <c.triat33@gmail.com>
2020-10-30 20:08:15 +0100

    creation navbar

--- site/src/components/Navbar.vue
+++ site/src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <b-navbar toggleable="lg" type="" variant="faded">
     <b-navbar-brand to="/">
 <<<<<<< HEAD
+<<<<<<< HEAD
       <img id="logo" src="./../assets/logo.png" @click="navToDefaultPage"/>
     </b-navbar-brand>
     <b-navbar-nav class="ml-auto text">
@@ -11,6 +12,11 @@
     </b-navbar-brand>
     <b-navbar-nav class="ml-auto text">
 >>>>>>> 9364028 (Rebase main)
+=======
+      <img id="logo" src="./../assets/logo.png"/>
+    </b-navbar-brand>
+    <b-navbar-nav class="ml-auto text">
+>>>>>>> ef2c739 (creation navbar)
       <b-nav-item to="/navbar" class=" mr-4">Créer mon compte</b-nav-item>
       <b-nav-item to="/navbar" class=" mr-4">Se connecter</b-nav-item>
     </b-navbar-nav>
@@ -19,6 +25,7 @@
 
 <script>
 export default {
+<<<<<<< HEAD
 <<<<<<< HEAD
   name: "navbar",
   methods: {
@@ -32,6 +39,9 @@ export default {
 =======
   name: "navbar"
 >>>>>>> 9364028 (Rebase main)
+=======
+  name: "navbar"
+>>>>>>> ef2c739 (creation navbar)
 };
 </script>
 
