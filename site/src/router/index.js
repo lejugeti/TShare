4deb280804504b504e1c70052220e152dbf94ee4
ctriat <c.triat33@gmail.com>
2020-11-29 16:33:59 +0100

    barre categories

--- site/src/router/index.js
+++ site/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import AffVetement from "@/components/AffichageVetement";
+import RechercheVetement from "@/components/RechercheVetement";
 
 Vue.use(Router);
 
@@ -9,10 +10,11 @@ export default new Router({
   routes: [
     {
       path: "/",
+      component: RechercheVetement,
     },
     {
       path: "/affVetement/:id",
-      component: AffVetement,
+      component: AffVetement
     },
     {
       path: "*",
