06b68ef9e58d5ef4690dfb9d5ce7893271606751
Paul Lorgue <paullorgue@gmail.com>
2020-12-25 23:20:29 +0100

    Profil front toggle notif et confidentialité

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -59,9 +59,37 @@
             </div>
           </div>
         </div>
-        <div class="section" v-if="selected === 5" >
+        <div class="section displayColomn" v-if="selected === 5" >
+          <div class="displayRow">
+            <p>Demande de location reçue</p>
+            <label class="switch">
+              <input type="checkbox" checked>
+              <span class="slider round"></span>
+            </label>
+          </div>
+          <div class="displayRow">
+            <p>Demande de location validée par le propriétaire</p>
+            <label class="switch">
+              <input type="checkbox" checked>
+              <span class="slider round"></span>
+            </label>
+          </div>
+          <div class="displayRow">
+            <p>Rappel d’envoi</p>
+            <label class="switch">
+              <input type="checkbox" checked>
+              <span class="slider round"></span>
+            </label>
+          </div>
         </div>
-        <div class="section" v-if="selected === 6" >
+        <div class="section displayColomn" v-if="selected === 6" >
+          <div class="displayRow">
+            <p>Autoriser T-Share à personnaliser mon fil d’actu et mes résultats de recherche en évaluant mes préférences, mes paramètres, mes locations précédentes ainsi que mon utilisation du site Web T-Share</p>
+            <label class="switch">
+              <input type="checkbox" checked>
+              <span class="slider round"></span>
+            </label>
+          </div>
         </div>
       </div>
     </div>
@@ -281,6 +309,73 @@ export default {
 }
 .margin-right{
   margin-right: 6px;
+}
+
+/* Switch */
+.displayColomn{
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+}
+.displayRow{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-right: 7%;
+  max-width: 700px;
+}
+.displayRow p {
+  max-width: calc(100% - 80px);
+}
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 45px;
+  height: 22px;
+}
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 15px;
+  width: 15px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+input:checked + .slider {
+  background-color: #B8D6AA;
+}
+input:focus + .slider {
+  box-shadow: 0 0 1px #B8D6AA;
+}
+input:checked + .slider:before {
+  -webkit-transform: translateX(22px);
+  -ms-transform: translateX(22px);
+  transform: translateX(22px);
+}
+.slider.round {
+  border-radius: 34px;
+}
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
 
