780c95803ce5c987d53bc03af3b09823df0ca867
ctriat <c.triat33@gmail.com>
2021-01-04 17:29:09 +0100

    correction conflit

--- site/src/components/Navbar.vue
+++ site/src/components/Navbar.vue
@@ -1,22 +1,11 @@
 <template>
   <b-navbar toggleable="lg" type="" variant="faded">
     <b-navbar-brand to="/">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       <img id="logo" src="./../assets/logo.png" @click="navToDefaultPage"/>
     </b-navbar-brand>
     <b-navbar-nav class="ml-auto text">
       <b-nav-item to="/navbar" class=" mr-4" @click="navToAjoutPage">Ajouter un vêtement</b-nav-item>
-=======
-      <img id="logo" src="./../assets/logo.png"/>
-    </b-navbar-brand>
-    <b-navbar-nav class="ml-auto text">
->>>>>>> 9364028 (Rebase main)
-=======
-      <img id="logo" src="./../assets/logo.png"/>
-    </b-navbar-brand>
-    <b-navbar-nav class="ml-auto text">
->>>>>>> ef2c739 (creation navbar)
       <b-nav-item to="/navbar" class=" mr-4">Créer mon compte</b-nav-item>
       <b-nav-item to="/navbar" class=" mr-4">Se connecter</b-nav-item>
     </b-navbar-nav>
@@ -25,8 +14,6 @@
 
 <script>
 export default {
-<<<<<<< HEAD
-<<<<<<< HEAD
   name: "navbar",
   methods: {
     navToDefaultPage(){
@@ -36,12 +23,6 @@ export default {
       this.$router.push("/ajoutVetement")
     }
   }
-=======
-  name: "navbar"
->>>>>>> 9364028 (Rebase main)
-=======
-  name: "navbar"
->>>>>>> ef2c739 (creation navbar)
 };
 </script>
 
