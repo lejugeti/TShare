90c0036be0cd158cd8aaed4355ed466e14fad55e
Antoine PARIZE <45354189+lejugeti@users.noreply.github.com>
2021-01-04 15:22:53 +0100

    Ajout page historique

--- site/src/router/index.js
+++ site/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from "vue-router";
 import AffVetement from "@/components/AffichageVetement";
 import RechercheVetement from "@/components/RechercheVetement";
 import AjoutVetement from "@/components/AjouterVetement";
+import HistoriqueView from "@/components/HistoriqueView";
+
 
 Vue.use(VueRouter);
 
@@ -13,6 +15,7 @@ const router = new VueRouter({
     { path: "/affVetement/:id", component: AffVetement },
     { path: "/ajoutVetement/", component: AjoutVetement },
     { path: "/rechercheVetement/", component: RechercheVetement },
+    { path: "/historique/", component: HistoriqueView },
     { path: "*", redirect: "/" }
   ]
 });
